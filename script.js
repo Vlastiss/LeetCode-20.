c0b18104edcd05e29e905ab437d32e7ee9c59a35
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-02-04 20:57:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,37 @@
  
 
 // Example 1:
-
 // Input: s = "()"
 // Output: true
-// Example 2:
 
+// Example 2:
 // Input: s = "()[]{}"
 // Output: true
-// Example 3:
 
+// Example 3:
 // Input: s = "(]"
 // Output: false
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+
+var isValid = function(s) {
+
+    const pairs =  {
+        pair1: "()",
+        pair2: "[]",
+        pair3: "{}"
+    }
+    const brackets = [...s];
+    
+    for (let i =0; i <brackets.length; i++) {
+
+        // console.log(brackets[i])
+        console.log(brackets[i + 1])
+    }
+};
+
+isValid("()[]");
